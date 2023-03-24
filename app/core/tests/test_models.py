c151eec9170710b3bce540faa40bf3eff1a9b241
victor.castillo.harrington <victor.castillo.harrington@gmail.com>
2023-03-24 11:51:12 -0500

    init custom user models

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 """
-Tests for models
+Test for models
 """
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -8,10 +9,10 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test Models"""
 
-    def create_user_with_email_successful(self):
+    def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = "test@example.com"
-        password = "testpassword123"
+        password = "testpass123"
         user = get_user_model().objects.create_user(
             email=email,
             password=password
