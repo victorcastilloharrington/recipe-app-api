97caa5c1f1afda6f5e62b713d3de25bb9c7928ee
victor.castillo.harrington <victor.castillo.harrington@gmail.com>
2023-04-12 12:07:30 -0500

    added ingredient models

--- app/core/models.py
+++ app/core/models.py
@@ -82,3 +82,15 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredients for recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
